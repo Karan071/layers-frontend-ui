85ac568d37ffdbac002eb87d54915f555cd0cf06
Karan <kc2532002@gmail.com>
2025-03-19 16:58:24 +0530

    footer

--- frontend/src/sections/Footer.tsx
+++ frontend/src/sections/Footer.tsx
@@ -1,3 +1,6 @@
+import Logo from "@/assets/images/logo.svg";
+import Image from "next/image";
+
 const footerLinks = [
     { href: "#", label: "Contact" },
     { href: "#", label: "Privacy Policy" },
@@ -5,5 +8,28 @@ const footerLinks = [
 ];
 
 export default function Footer() {
-    return <div>Footer</div>;
+    return <section className="py-16">
+        <div className="container">
+            <div className="flex flex-col md:flex-row items-center gap-6 md:justify-between">
+                <div>
+                    <Image
+                        src={Logo}
+                        alt={`layers ${Logo}`}
+                    />
+                </div>
+                <div>
+                    <nav className="flex gap-6">
+                        {footerLinks.map((link) => (
+                            <a 
+                                href={link.href}
+                                className="text-white/50 text-sm"
+                                >
+                                {link.label}
+                            </a>
+                        ))}
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </section>;
 }
